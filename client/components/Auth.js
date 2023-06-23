01be15ee05c65baeaec4f77711d87a0f1480b418
Robert Hoover <peelintaters85@gmail.com>
2023-06-23 03:39:53 -0700

    minor changes

--- client/components/Auth.js
+++ client/components/Auth.js
@@ -11,10 +11,10 @@ const Auth = () => {
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
 
-    const [variant, setVariant] = useState('login');
+    const [view, setView] = useState('login');
 
-    const toggleVariant = useCallback(() => {
-        setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login');
+    const toggleView = useCallback(() => {
+        setView((currentView) => currentView === 'login' ? 'register' : 'login');
     }, []);
 
     const login = useCallback(async () => {
@@ -35,10 +35,10 @@ const Auth = () => {
           <div className="flex justify-center">
             <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
               <h2 className="text-white text-2xl mb-8 font-semibold">
-                {variant === 'login' ? 'Sign in' : 'Register'}
+                {view === 'login' ? 'Sign in' : 'Register'}
               </h2>
               <div className="flex flex-col gap-4">
-                {variant === 'register' && (
+                {view === 'register' && (
                   <Input
                     label="Username"
                     onChange={((e) => setName(e.target.value))}
@@ -61,8 +61,8 @@ const Auth = () => {
                   value={password}
                 />
               </div>
-              <button onClick={variant === "login" ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
-                {variant === "login" ? "Login" : "Sign up"}
+              <button onClick={view === "login" ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                {view === "login" ? "Login" : "Sign up"}
               </button>
               <div className="flex flex-row items-center gap-4 mt-8 justify-center"> 
                 <div className="
@@ -95,9 +95,9 @@ const Auth = () => {
                     </div>
               </div>
                 <p className="text-neutral-500 mt-12 text-sm">
-                  {variant === "login" ? "First time using Rate the Meme?" : "Already have an account?"}
-                  <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
-                    {variant === "login" ? "Create an account" : "Login"}
+                  {view === "login" ? "First time using Rate the Meme?" : "Already have an account?"}
+                  <span onClick={toggleView} className="text-white ml-1 hover:underline cursor-pointer">
+                    {view === "login" ? "Create an account" : "Login"}
                   </span>        
                 </p>
               </div>
